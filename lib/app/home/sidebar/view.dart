2e277e93dc2f96993d2ae03d19945f5acca8ed3c
licheng <2501093733@qq.com>
2024-08-22 10:29:06 +0800

    暂时关闭手风琴动画

--- lib/app/home/sidebar/view.dart
+++ lib/app/home/sidebar/view.dart
@@ -105,7 +105,7 @@ class SidebarPage extends StatelessWidget {
               visible: logic.expansionTile.contains(item.name),
               child: Column(
                 children: item.children.map((e) => _text(e,left: 24)).toList(),
-              ).  toAccordion(true)
+              )
           );
         })
       ],
