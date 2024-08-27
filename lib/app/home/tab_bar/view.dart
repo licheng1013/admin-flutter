0ee5b800bba403ef27fc0a724919b048142bd27c
licheng <2501093733@qq.com>
2024-08-27 09:49:44 +0800

    调整指示器动画

--- lib/app/home/tab_bar/view.dart
+++ lib/app/home/tab_bar/view.dart
@@ -28,6 +28,7 @@ class TabBarPage extends StatelessWidget {
         children: [
           TabBar(
             controller: logic.tabController,
+            indicatorAnimation: TabIndicatorAnimation.linear,
             onTap: (e) {
               var tab = logic.tabList[e];
               logic.currentIndex.value = e;
