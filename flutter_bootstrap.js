03023baea7f79efbac1299d2f72cba395843ba6c
licheng1013 <licheng1013@users.noreply.github.com>
2024-09-12 03:22:12 +0000

    deploy: f8f3095f7fc8ef2077245cf64ebc1a2394d49a06

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"7bf1169715d69fee5f421829d308bd0464337d9e","builds":[{"compileTarget":"dart2js","renderer":"html","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"ee5adf6d2ee1f14dc0a5128a1caa5d0b5f19e6a2","builds":[{"compileTarget":"dart2js","renderer":"html","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1814778879"
+    serviceWorkerVersion: "2848128594"
   }
 });
